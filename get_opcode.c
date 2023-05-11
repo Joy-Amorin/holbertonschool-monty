a03c7e61b5e6e34933a6f934edeb60e5baf449a1
Joy-Amorin <amorinkaren2021@gmail.com>
2023-05-11 11:20:56 -0700

    remove freestack

--- get_opcode.c
+++ get_opcode.c
@@ -1,24 +1,6 @@
 #include "monty.h"
 
 /**
- * isNumber - checks if a string is a number
- * @str_n: string to check
- *
- * Return: 1 if its a number, 0 otherwise.
- */
-int isNumber(char str_n[])
-{
-	int i = 0;
-
-	if (str_n[0] == '-')
-		str_n[0] = '0';
-	for (i = 0; str_n[i]; i++)
-		if (!isdigit(str_n[i]))
-			return (0);
-	return (1);
-}
-
-/**
  * get_opcode - get opcode function
  * @token: name
  *
@@ -78,5 +60,5 @@ int main(int argc, char **argv)
 	}
 	free(buffer);
 	fclose(file);
-/*	free_stack(stack);*/
+	/*free_stack(stack);*/
 	return (0); }
