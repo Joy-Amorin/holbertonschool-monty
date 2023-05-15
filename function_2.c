94c06e5472aa8b49a20a95f22e1a55365d47344a
Macj04 <miguelcoa49@gmail.com>
2023-05-15 09:52:49 -0700

    add the function swap to the file functions_2.c

--- function_2.c
+++ function_2.c
@@ -34,3 +34,23 @@ void _pop(stack_t **stack, unsigned int line_number)
 	*stack = (*stack)->next;
 	free(temp);
 }
+
+/**
+ * _swap - function that swap a top of and second top from stack
+ * @stack: pointer to linked list stack
+ * @line_number: line opcode number
+ */
+
+void _swap(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		return;
+	}
+
+	int tmp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = tmp;
+}
+
