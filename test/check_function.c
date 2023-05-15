85adba4c5d9bfdd350c0b5958dcec8a6868131b5
Macj04 <miguelcoa49@gmail.com>
2023-05-15 07:56:28 -0700

    create the function that checks opcode

---
+++ test/check_function.c
@@ -0,0 +1,34 @@
+#include "monty.h"
+
+/**
+ * func_cheq - Function that  checks opcode and returns the correct function
+ * @str: the opcode
+ * Return: returns a function, or NULL on failure
+ */
+
+instruct_func func_cheq(char *str)
+{
+	int i;
+
+
+	instruction_t instruct[] = {
+		{"push", _push},
+		{"pall", _pall},
+		{"pint", _pint},
+		{"pop", _pop},
+		{"swap", _swap},
+		{NULL, NULL},
+	};
+
+	i = 0;
+	while (instruct[i].f != NULL)
+	{
+		if (str != NULL && strcmp(instruct[i].opcode, str) == 0)
+		{
+			return (instruct[i].f);
+		}
+		i++;
+	}
+
+	return (instruct[i].f);
+}
